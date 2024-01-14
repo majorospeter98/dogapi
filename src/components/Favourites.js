53976ea55a65f45f36b8586c2cab5fa8d5abab10
Majoros Péter <majoros.peter98@gmail.hu>
2024-01-14 21:06:14 +0100

    updates

--- src/components/Favourites.js
+++ src/components/Favourites.js
@@ -1,33 +1,40 @@
 import { useEffect, useState } from "react";
-import { config,deleteConfig } from '../Key'
+import { config, deleteConfig } from "../Key";
 import axios from "axios";
-import '../index.css';
-import DeleteIcon from '@mui/icons-material/Delete';
-function Favourites (){
-    const [favourite,setFavourite]= useState(null);
-    const getFavourites = () =>{
-        axios.get("https://api.thedogapi.com/v1/favourites?sub_id=Test",config)
-        .then(response =>setFavourite((response.data)))
-            } 
-    const delFavourites = (e,id) =>{
-        e.preventDefault();
-        axios.delete(`https://api.thedogapi.com/v1/favourites/${id}`,deleteConfig)
-        .then(response => console.log(response.data));
-            }
-    useEffect(() =>{
-getFavourites();
-    }, [favourite])
-    return (
-                <>
-                <div className="flex container">
-{favourite?.map(favs=>(
-        <div className="flex">
-    <img src={favs.image.url}/>
-    <DeleteIcon className="delete" style={{fontSize:"50px"}} alt="Törlés" onClick={(e) => delFavourites(e,favs.id)}/>
-        </div>
-))} 
-</div>
-        </>
-    )
+import "../index.css";
+import DeleteIcon from "@mui/icons-material/Delete";
+function Favourites() {
+  const [favourite, setFavourite] = useState(null);
+  const getFavourites = () => {
+    axios
+      .get("https://api.thedogapi.com/v1/favourites?sub_id=Test", config)
+      .then((response) => setFavourite(response.data));
+  };
+  const delFavourites = (e, id) => {
+    e.preventDefault();
+    axios
+      .delete(`https://api.thedogapi.com/v1/favourites/${id}`, deleteConfig)
+      .then((response) => (response.data));
+  };
+  useEffect(() => {
+    getFavourites();
+  }, [favourite]);
+  return (
+    <>
+      <div className="flex-container">
+        {favourite?.map((favs) => (
+          <div className="flex" key={favs.id}>
+            <img src={favs.image.url} alt="dog images"/>
+            <DeleteIcon
+              className="delete"
+              style={{ fontSize: "50px" }}
+              alt="Törlés"
+              onClick={(e) => delFavourites(e, favs.id)}
+            />
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
 export default Favourites;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,62 +1,66 @@
 import { useState } from "react";
 import axios from "axios";
 import { useEffect } from "react";
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import RestartAltIcon from '@mui/icons-material/RestartAlt';
-import { config } from '../Key'
-import {Routes,Route} from 'react-router-dom';
-function Home(){
-    const [dog, setDog] = useState(null);
-    
-    function fetch() {
-      axios
-        .get("https://api.thedogapi.com/v1/images/search?limit=10", config)
-        .then((response) => setDog(response.data))
-        .catch((e) => console.error("error during api get"));
-    }
-    const postFav = (e, dogId) => {
-      e.preventDefault();
-      const postOptions = {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "x-api-key":
-            "live_s43BDT8YZ1TkvY0aazwbgauJwgbZET8ltkxQob2TQtijTTX76KacNRIYsAzeg2Y9",
-        },
-      };
-    
-      const postdata = {
-        image_id: dogId,
-        sub_id: "Test",
-      };
-      axios
-        .post("https://api.thedogapi.com/v1/favourites", postdata, postOptions)
-        .then((response) => console.log(response.data))
-        .catch((error) => {
-          console.error(error);
-        });
-      }
-    
-    useEffect(() => {
-      fetch();
-    }, []);
-    
-    return (
-      <div>
-        <div className="flex container">
-          {dog?.map((dogs) => (
-            <div className="flex">
-              <img src={dogs.url} />
-              <FavoriteIcon style={{fontSize:"50px"}} onClick={(e) => postFav(e, dogs.id)} className="icon" />
-            </div>
-          ))}
-        </div>
-        <div className="center">
-          Regenerate 10 dog image:
-          <RestartAltIcon className="restart" style={{fontSize:"50px"}} onClick={fetch} />
-        </div>
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import RestartAltIcon from "@mui/icons-material/RestartAlt";
+import { config } from "../Key";
+import { Routes, Route } from "react-router-dom";
+function Home() {
+  const [dog, setDog] = useState(null);
+  function fetch() {
+    axios
+      .get("https://api.thedogapi.com/v1/images/search?limit=10", config)
+      .then((response) => setDog(response.data))
+      .catch((e) => console.error("error during api get"));
+  }
+  const postFav = (e, dogId) => {
+    e.preventDefault();
+    const postOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "x-api-key":
+          "live_s43BDT8YZ1TkvY0aazwbgauJwgbZET8ltkxQob2TQtijTTX76KacNRIYsAzeg2Y9",
+      },
+    };
+    const postdata = {
+      image_id: dogId,
+      sub_id: "Test",
+    };
+    axios
+      .post("https://api.thedogapi.com/v1/favourites", postdata, postOptions)
+      .then((response) => response.data)
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+  useEffect(() => {
+    fetch();
+  }, []);
+  return (
+    <div>
+      <div className="flex-container">
+        {dog?.map((dogs) => (
+          <div className="flex" key={dogs.id}>
+            <img src={dogs.url} alt="dog images" />
+            <FavoriteIcon
+              style={{ fontSize: "50px" }}
+              onClick={(e) => postFav(e, dogs.id)}
+              className="icon"
+            />
+          </div>
+        ))}
       </div>
-    );
-} 
+      <div className="center">
+        Regenerate 10 dog image:
+        <RestartAltIcon
+          className="restart"
+          style={{ fontSize: "50px" }}
+          onClick={fetch}
+        />
+      </div>
+    </div>
+  );
+}
 
 export default Home;
